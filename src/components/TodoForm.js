dbbf19c98dac5bad247188066294f74351d5c20c
AMIYA RANJAN RANASINGH <117471329+ranasinghamiya@users.noreply.github.com>
2023-09-29 23:58:49 +0530

    Update TodoForm.js

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,13 +1,28 @@
+// components/TodoForm.js
+
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addTodo } from '../redux/actions';
 
 const TodoForm = () => {
-  //Write a code here as needed
+  const [task, setTask] = useState('');
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addTodo(task));
+    setTask('');
+  };
+
   return (
-    <form>
-      <input type='text' placeholder='Add a task...' />
-      <button type='submit'>Add</button>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+        placeholder="Add a new task"
+      />
+      <button type="submit">Add Task</button>
     </form>
   );
 };
