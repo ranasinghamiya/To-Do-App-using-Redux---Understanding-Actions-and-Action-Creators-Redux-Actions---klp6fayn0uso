91700d9d2adf49ce8c7aa535d253c352238f2928
AMIYA RANJAN RANASINGH <117471329+ranasinghamiya@users.noreply.github.com>
2023-09-30 00:03:26 +0530

    Update TodoItem.js

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,17 +1,28 @@
+
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { toggleTodo, deleteTodo } from '../redux/actions';
 
-const TodoItem = ({ todo }) => {
-  //Write a code as per your need
+const TodoItem = ({ id, task, completed }) => {
+  const dispatch = useDispatch();
+
+  const handleToggle = () => {
+    dispatch(toggleTodo(id));
+  };
+
+  const handleDelete = () => {
+    dispatch(deleteTodo(id));
+  };
+
   return (
     <li>
       <span
-        style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
+        style={{ textDecoration: completed ? 'line-through' : 'none' }}
+        onClick={handleToggle}
       >
-        {todo.text}
+        {task}
       </span>
-      <button>Delete</button>
+      <button onClick={handleDelete}>Delete</button>
     </li>
   );
 };
