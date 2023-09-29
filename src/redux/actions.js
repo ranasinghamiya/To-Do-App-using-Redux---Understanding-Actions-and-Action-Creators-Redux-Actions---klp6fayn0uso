df2f33723db022d504e11da46c36f42a644cf025
AMIYA RANJAN RANASINGH <117471329+ranasinghamiya@users.noreply.github.com>
2023-09-30 00:01:27 +0530

    Update actions.js

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,13 +1,20 @@
-let nextTodoId = 1;
 
-export const addTodo = (text) => ({
-  //
+
+export const ADD_TODO = 'ADD_TODO';
+export const TOGGLE_TODO = 'TOGGLE_TODO';
+export const DELETE_TODO = 'DELETE_TODO';
+
+export const addTodo = (task) => ({
+  type: ADD_TODO,
+  payload: task
 });
 
 export const toggleTodo = (id) => ({
-  //
+  type: TOGGLE_TODO,
+  payload: id
 });
 
 export const deleteTodo = (id) => ({
-  //
+  type: DELETE_TODO,
+  payload: id
 });
